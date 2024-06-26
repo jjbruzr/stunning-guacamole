594bf6377be39f78b612e0497aeee0fa873bff0a
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-06-26 14:00:18 -0400

    + importing response

---
+++ people_in_space2.py
@@ -0,0 +1,9 @@
+from urllib import response
+import requests
+
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json()
+
+print('The people currently in space are:')
+for person in json['people']:
+    print(person['name'])
