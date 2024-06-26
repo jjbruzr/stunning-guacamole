2b0bef50f581a8e9fe0e9e7f70ba2451271b5b0b
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-06-26 13:59:40 -0400

    cleaned up the second stanza so it prints everything in the people key value pair

--- people_in_space1.py
+++ people_in_space1.py
@@ -3,8 +3,4 @@ import requests
 
 response = requests.get('http://api.open-notify.org/astros.json')
 json = response.json()
-# print(json)
-
-print('The people currently in space are:')
-for person in json['people']:
-    print(person['name'])
+print(json)
