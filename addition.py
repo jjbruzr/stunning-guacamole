0dd0d734a07496f185023f5acda0cdc170a9ea50
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-06-27 09:24:41 -0400

    used function to add two numbers

---
+++ addition.py
@@ -0,0 +1,13 @@
+# further investigation of functions
+
+def addition(a, b):
+    return a + b
+
+
+# Main program
+num1 = float(input('Enter your 1st number:\n'))
+num2 = float(input('Enter your 2nd number:\n'))
+
+# call our addition function
+result = addition(num1, num2)
+print('The result is', result)
