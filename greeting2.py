59535e76578f00daa2a3fe8af14fa5d94c3d3213
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-06-27 09:13:45 -0400

    + function practice

---
+++ greeting2.py
@@ -0,0 +1,10 @@
+# working with functions.
+
+# define my greeting function
+def greeting(name):
+    print('Hello', name)
+
+# Main program
+input_name = input('Enter your name:\n')
+
+greeting(input_name)
