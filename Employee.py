85c66facb27a94512939fb84b4377dae031688e2
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-07-04 11:38:08 -0400

    def employee class and function to calculate paycheck

---
+++ Employee.py
@@ -0,0 +1,11 @@
+# This is the class demo creating a payroll system.
+class Employee:
+    def __init__(self, fname, lname, salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+
+    def calculate_paycheck(self):
+        return self.salary/52
+    
+    
