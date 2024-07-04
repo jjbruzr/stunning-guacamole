6f7ff9fd8f68f71630201adca3f0373c970a9f01
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-07-04 11:40:35 -0400

    + remarks

--- Employee.py
+++ Employee.py
@@ -1,11 +1,13 @@
 # This is the class demo creating a payroll system.
+
+# create the Employy class and initialize it's properties
 class Employee:
     def __init__(self, fname, lname, salary):
         self.fname = fname
         self.lname = lname
         self.salary = salary
 
+# create a method to calculate weekly paychecm amount
     def calculate_paycheck(self):
         return self.salary/52
-    
     
