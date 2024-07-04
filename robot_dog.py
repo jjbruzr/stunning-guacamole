17d498d27a5543309cbc5b49f093321c94b184eb
Jason Abruzzo <jason.abruzzo@gmail.com>
2024-07-04 11:25:46 -0400

    use bark method

--- robot_dog.py
+++ robot_dog.py
@@ -2,7 +2,7 @@ class Robot_Dog:
 	def __init__(self, name_val, breed_val):
 		self.name = name_val
 		self.breed = breed_val
-		
+
 	def bark(self):
 		print('Woof Woof')
 
@@ -12,3 +12,4 @@ class Robot_Dog:
 my_dog = Robot_Dog('Spot', 'Chihuaha')
 print(my_dog.name)
 print(my_dog.breed)
+my_dog.bark()
